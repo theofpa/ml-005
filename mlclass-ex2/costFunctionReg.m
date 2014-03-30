176497f212d9ad2246e9e41258d2226b670c153e
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-03-30 14:23:46 +0200

    Exercise 2: Regularized LR

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,11 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J=((1/m)*sum(-y.*log(sigmoid(X*theta))-((1-y).*log(1-sigmoid(X*theta)))))+((lambda/(2*m))*sum(theta(2:end,1).^2))  ;
 
-
-
-
-
+grad(1,1)=(1/m)*(( sigmoid( theta' * X' ) - y' ) * X(:,1));
+grad(2:end,1)=((1/m)*(( sigmoid( theta' * X' ) - y' ) * X(:,2:end)))'+(lambda/m)*theta(2:end);
 % =============================================================
 
 end
